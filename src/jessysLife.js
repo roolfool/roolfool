9a07ddbfb964348731e21870552db95626e46850
said <madi888364@gmail.com>
2023-10-13 14:20:45 +0300

    task1.2

--- src/jessysLife.js
+++ src/jessysLife.js
@@ -1,5 +1,3 @@
-console.log(Say my name.);
-
-console.log(`You're Heisenberg, right?')
-
-console.log('You're god damn right)')
+console.log("Say my name");
+console.log("You're Heisenberg, right?");
+console.log("You're god damn right)");
